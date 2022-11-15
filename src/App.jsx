316fdbf7c1edf098f0527fecb1f80988765f9db7
melakualehegn <melakualehegn34@gmail.com>
2022-11-16 01:00:10 +0300

    navigation bar aded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Navabar from "./components/Navabar"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navabar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navabar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navabar() {
+  return (
+    <div className='nav'>
+      <img src='src/Path.png'/>
+      <h3>my travel journal</h3>
+    </div>
+  )
+}
+
+export default Navabar
